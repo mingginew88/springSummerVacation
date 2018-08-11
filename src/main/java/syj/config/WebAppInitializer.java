a8a579ece332d1d1891e62fa27811c027137d7a8
Dean <mingginew88@gmail.com>
2018-08-11 15:56:26 +0900

    commit spring studyˆ

---
+++ src/main/java/syj/config/WebAppInitializer.java
@@ -0,0 +1,24 @@
+package syj.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(WebMvcConfiguration.class);
+        ctx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+  <title>SYJ Spring Study</title>
+
+  <link rel="stylesheet" type="text/css" href="/resources/css/common.css"/>
+  <script type="text/javascript" src="/resources/js/common.js"></script>
+
+</head>
+<body>
+  <p>SYJ Spring Study</p>
+</body>
+</html>
